a1b6aa5ffedac7db0a4092bc01c77000f10d3bdb
Alejandro Osornio <50227494+AOx0@users.noreply.github.com>
2022-08-16 20:49:09 -0500

    Dynamic push

--- pila_rs_unsafe/src/main.rs
+++ pila_rs_unsafe/src/main.rs
@@ -1,8 +1,8 @@
 #![allow(dead_code)]
 
 use std::{
-    alloc::{alloc, dealloc, Layout},
-    fmt::Display,
+    alloc::{alloc, dealloc, Layout, realloc},
+    fmt::{Display, Debug},
     marker::PhantomData, mem::MaybeUninit,
 };
 
@@ -21,22 +21,22 @@ impl<T: Sized> Pila<T> {
 
     pub fn new(size: usize) -> Self {
         unsafe {
-            let pila = Pila {
+            Pila {
                 layout: Layout::array::<MaybeUninit<T>>(size).unwrap(),
                 memoria: alloc(Layout::array::<MaybeUninit<T>>(size).unwrap()),
                 maximum: size,
                 current: 0,
                 ty: PhantomData,
-            };
-            
-            let iter = pila.get_slice();
+            }
+        }
+    }
 
-            for i in 0..size {
-                iter[i] = std::mem::MaybeUninit::uninit();
-            };
-
-            pila
-
+    pub fn fix(&mut self) {
+        unsafe {
+            let new = Layout::array::<MaybeUninit<T>>(self.current).unwrap();
+            self.memoria = realloc(self.memoria, self.layout, new.size());
+            self.maximum = self.current;
+            self.layout = new;
         }
     }
 
@@ -66,12 +66,15 @@ impl<T: Sized> Pila<T> {
     pub fn push(&mut self, value: T) -> Result<(), ()> {
         unsafe {
             if self.current == self.maximum {
-                Err(())
-            } else {
-                self.get_slice()[self.current].write(value);
-                self.current += 1;
-                Ok(())
-            }
+                self.maximum += 1;
+                let new = Layout::array::<MaybeUninit<T>>(self.maximum).unwrap();
+                self.memoria = realloc(self.memoria, self.layout, new.size());
+                self.layout = new;
+            } 
+            self.get_slice()[self.current].write(value);
+            self.current += 1;
+            Ok(())
+            
         }
     }
 }
@@ -89,7 +92,7 @@ impl<T: Display + Clone + Default> Display for Pila<T> {
             for i in 0..self.maximum {
                 match i {
                     x if x < self.current => write!(f, " {:4} |", slice[x].assume_init_ref())?,
-                    _ => write!(f, " {:4} |", "null")?,
+                    _ => (),
                 }
             }
         }
@@ -98,19 +101,36 @@ impl<T: Display + Clone + Default> Display for Pila<T> {
     }
 }
 
+impl<T: Debug> Debug for Pila<T> {
+   fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
+       unsafe {
+            let slice = self.get_slice(); 
+            for i in 0..self.maximum {
+                match i {
+                    x if x < self.current => write!(f, " {:4?} |", slice[x].assume_init_ref())?,
+                    _ => write!(f, " {:4} |", "null")?,
+                }
+            }
+        }
+
+        Ok(())
+
+   } 
+}
+
 fn main() {
-    let mut pila: Pila<String> = Pila::new(10);
+    let mut pila: Pila<&str> = Pila::new(0);
     let mut i = 1;
-    while let Ok(_) = pila.push("Hola".to_owned()) {
-        print!("Agregado : {:4} -- ", i * 10);
-        println!("{pila}");
+    while let Ok(_) = pila.push("Hola") {
+        println!("{pila:?}");
         i += 1;
+        if i == 10 {
+            break;
+        }
     }
 
-    *pila.top().unwrap() = "Test".to_owned();
-
-    while let Some(value) = pila.pop() {
-        print!("Eliminado: {value:4} -- ");
-        println!("{pila}");
+    while let Some(_) = pila.pop() {
+        pila.fix();
+        println!("{pila:?}");
     }
 }
