afb2dc57fb4115ff834b996be85cc78b2fd513ce
Alejandro D <alejandro@Mac.local>
2022-09-07 02:21:38 -0500

    Refactor

--- repaso/lifo.cpp
+++ repaso/lifo.cpp
@@ -89,4 +89,79 @@ bool Pila::is_empty() {
 
 bool Pila::is_full() {
   return curr == size;
+}
+
+void Pila::push_prime(int n) {
+  if (num_primos == size_num_primos) {
+    size_num_primos += 100;
+    int * new_primos = new int[size_num_primos];
+
+    for (int i=0; i<num_primos; i++) {
+      new_primos[i] = primos[i];
+    }
+
+    delete [] primos;
+
+    primos = new_primos;
+  }
+
+  primo_max = n;
+  primos[num_primos] = n;
+  num_primos++;
+}
+
+void Pila::gen_primos(int hasta) {
+  delete [] primos;
+  size_num_primos = 0;
+  num_primos = 0;
+
+  bool * no_primos = new bool[hasta+1];
+
+  for (int i=0; i<=hasta; i++) no_primos[i] = false;
+
+  for (int i=2; i<=hasta; i++) {
+    if (no_primos[i] != true) {
+      push_prime(i);
+      int j = 1;
+      while (true) {
+        int b = pow(i, 1) * j;
+        if (b > hasta) break;
+        no_primos[b] = true;
+        j++;
+      }
+    }
+  }
+
+  delete [] no_primos;
+}
+
+bool Pila::is_prime(int n) {
+  if (num_primos == 0 || n > primo_max) {
+    gen_primos(n < 10000 ? 10000 : n);
+  }
+
+  return num_in(primos, size_num_primos, n);
+}
+
+bool Pila::num_in(int *n, int size, int num) {
+  for (int i=0; i<size; i++) {
+    if (num == n[i]) {
+      return true;
+    }
+  }
+
+  return false;
+}
+
+ostream &operator<<(ostream &os, const Pila &e) {
+  os << "[";
+  for (int i=0; i<e.curr; i++) {
+    os << " " <<  e.data[i];
+  }
+  os << " ]";
+  return os;
+}
+
+int Pila::numero_primos() const {
+  return primos_ingresados;
 };
--- repaso/lifo.hpp
+++ repaso/lifo.hpp
@@ -18,71 +18,12 @@ struct Pila {
     int primo_max;
     size_t num_primos;
     size_t size_num_primos;
-
     int primos_ingresados;
 
-    bool num_in(int * n, int size, int num) {
-      for (int i=0; i<size; i++) {
-        if (num == n[i]) {
-          return true;
-        }
-      }
-
-      return false;
-    }
-
-    void push_prime(int n) {
-      if (num_primos == size_num_primos) {
-        size_num_primos += 100;
-        int * new_primos = new int[size_num_primos];
-
-        for (int i=0; i<num_primos; i++) {
-          new_primos[i] = primos[i];
-        }
-
-        delete [] primos;
-
-        primos = new_primos;
-      }
-
-      primo_max = n;
-      primos[num_primos] = n;
-      num_primos++;
-    }
-
-
-    void gen_primos(int hasta) {
-      delete [] primos;
-      size_num_primos = 0;
-      num_primos = 0;
-
-      bool * no_primos = new bool[hasta+1];
-
-      for (int i=0; i<=hasta; i++) no_primos[i] = false;
-
-      for (int i=2; i<=hasta; i++) {
-        if (no_primos[i] != true) {
-          push_prime(i);
-          int j = 1;
-          while (true) {
-            int b = pow(i, 1) * j;
-            if (b > hasta) break;
-            no_primos[b] = true;
-            j++;
-          }
-        }
-      }
-
-      delete [] no_primos;
-    }
-
-    bool is_prime(int n) {
-      if (num_primos == 0 || n > primo_max) {
-        gen_primos(n < 10000 ? 10000 : n);
-      }
-      
-      return num_in(primos, size_num_primos, n); 
-    }
+    static bool num_in(int * n, int size, int num);
+    void push_prime(int n);
+    void gen_primos(int hasta);
+    bool is_prime(int n);
 
   protected:
     int suma;
@@ -91,14 +32,7 @@ struct Pila {
     int curr;
     Result<uint8_t, uint8_t> resize(int new_size);
 
-    friend ostream& operator<< (ostream &os, const Pila &e) {
-      os << "[";
-      for (int i=0; i<e.curr; i++) {
-        os << " " <<  e.data[i];
-      }
-      os << " ]";
-      return os;
-    }
+    friend ostream& operator<< (ostream &os, const Pila &e);
 
   public:
     Result<uint8_t, uint8_t> get(size_t idx);
@@ -109,9 +43,6 @@ struct Pila {
     bool is_full();
     Result<uint8_t, uint8_t> push(int valor);
     Result<uint8_t, uint8_t> pop(int valor);
-
-    int numero_primos() {
-      return primos_ingresados;
-    }
+    int numero_primos() const;
 };
 
